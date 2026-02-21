14117826b9a54d6d072177695998d5349feed731
Shahd Mostafa <shahd.mostafa3711@gmail.com>
2026-02-21 18:34:20 +0200

    add navbar section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Registration} from './components/registration/registration'
 import {HeroComponent} from './hero/hero'
+import { NavbarComponent } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Registration,HeroComponent],
+  imports: [RouterOutlet,Registration,HeroComponent, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
