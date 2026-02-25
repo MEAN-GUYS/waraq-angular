7a72efa47a45aa4726485fa491043abb54a3886d
Karim-308 <2001118@eng.asu.edu.eg>
2026-02-25 14:39:49 +0200

    fixing merge conflict

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,6 @@ import { HomePage } from './components/home-page/home-page';
 import { Registration } from './components/registration/registration';
 import { CartComponent } from './cart/cart';
 
-import { BooksPage } from './components/books-page/books-page';
-
 export const routes: Routes = [
     {path : ""  ,component : HomePage},
    //{path : "books"  ,component : BooksPage},*/
