0ade46e4dfef79207e4b97b43410a0cf0db5839c
Karim-308 <2001118@eng.asu.edu.eg>
2026-02-26 16:04:58 +0200

    fixing imports in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,17 @@ import { BooksPage } from './components/books-page/books-page';
 import { Registration } from './components/registration/registration';
 import { OrdersPage } from './components/orders-page/orders-page';
 import { CartComponent } from './cart/cart';
+import { AdminPage } from './components/admin-page/admin-page';
+import { Login } from './components/login/login';
+import { noAuthGuard } from './guards/no-auth.guard';
+import { userGuard , adminGuard } from './guards/role.guard';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'register', component: Registration },
   { path: 'my-orders', component: OrdersPage },
   { path: "books", component: BooksPage },
-  { path: "cart", component: CartPage, canActivate: [userGuard] },
+  { path: "cart", component: CartComponent, canActivate: [userGuard] },
   { path: "admin", component: AdminPage, canActivate: [adminGuard] },
   { path: "register", component: Registration, canActivate: [noAuthGuard] },
   { path: "login", component: Login, canActivate: [noAuthGuard] },
