4a11c67a9474224438b6275ec9d0b5b405857363
Karim-308 <2001118@eng.asu.edu.eg>
2026-02-26 15:59:53 +0200

    fixing routing to books

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './components/home-page/home-page';
+import { BooksPage } from './components/books-page/books-page';
 import { Registration } from './components/registration/registration';
 import { OrdersPage } from './components/orders-page/orders-page';
 import { CartComponent } from './cart/cart';
@@ -7,6 +8,7 @@ import { CartComponent } from './cart/cart';
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'register', component: Registration },
+  { path: 'books', component: BooksPage },
   { path: 'my-orders', component: OrdersPage },
-  { path: 'cart', component: CartComponent }
+  { path: 'cart', component: CartComponent },
 ];
